01c41fa4dca1703f266ec37576ed77ac3c8b5519
Kocsis <tomaskocsis6@gamil.com>
2022-12-11 14:09:08 +0100

    gitignore

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
+//removes unnecessary spaces in the input field
 function trimTextFieldCallback( event ) {
     const $textField = event.target;
     $textField.value = $textField.value.trim();
 }
 
-
-
 document.querySelectorAll('.js_text')
       .forEach(text => text.addEventListener('blur', trimTextFieldCallback));
 
-
+//in mobile view, it is closed by clicking on the navigation
 document.querySelector('.js-navlist').addEventListener("click", () => {
     document.querySelector('.js-checkbox').checked = false;
 })
